116e684b444214ee996ae92be6d61f0d2c3329a9
Michal Gutowski <45829609+michalgutowski@users.noreply.github.com>
2021-11-07 19:09:49 +0100

    Adding EPEL repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,9 @@ printf '192.168.99.100 podman\n' | sudo tee -a /etc/hosts > /dev/null
 # Upgrading Oracle Linux box with latest packages
 sudo dnf -y update
 
+# Installing EPEL repo
+sudo dnf -y install oracle-epel-release-el8
+
 # Installing container tools
 sudo dnf -y install podman podman-docker podman-remote podman-compose skopeo buildah runc git jq net-tools bind-utils
 
